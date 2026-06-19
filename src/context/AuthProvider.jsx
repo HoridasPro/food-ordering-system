430de40dafcb25b0d882cf2232716548067bcd7d
HoridasPro <horidassarker6@gmail.com>
2026-06-19 16:41:55 +0600

    added to home,footer, mainLayout

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,58 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { AuthContext } from "../AuthContext/AuthContext";
-import {
-  createUserWithEmailAndPassword,
-  onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut,
-  updateProfile,
-} from "firebase/auth";
-import { auth } from "../../firebase/firebase.init";
+import { AuthContext } from "./AuthContext";
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  // for the registration
-  const registerUser = (email, password) => {
-    setLoading(true);
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
-  // for the user Login
-  const userLogin = (email, password) => {
-    setLoading(true);
-    return signInWithEmailAndPassword(auth, email, password);
-  };
-  // For the profile update
-  const userProfileUpdate = (profile) => {
-    setLoading(true);
-    return updateProfile(auth.currentUser, profile);
-  };
-  // for the logout
-  const logOutUser = () => {
-    setLoading(true);
-    return signOut(auth);
-  };
-  // for the onauth change
-  useEffect(() => {
-    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-    return () => {
-      unSubscribe;
-    };
-  }, []);
-
-  const authInfo = {
-    user,
-    loading,
-    registerUser,
-    userProfileUpdate,
-    userLogin,
-    logOutUser,
-  };
-  return <AuthContext value={authInfo}>{children}</AuthContext>;
+  return <AuthContext>{children}</AuthContext>;
 };
 
 export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AuthProvider from "./context/AuthProvider";
+import router from "./Routes/MainRoute.jsx";
+import { RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
